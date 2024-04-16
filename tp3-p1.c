51616b7d665e844aad87e84b623a88e19d80b4b5
Lautaro Cruz <lautaro.cruzsrur@gmial.com>
2024-04-16 12:41:04 -0300

    finalizado los promedios

--- tp3-p1.c
+++ tp3-p1.c
@@ -2,25 +2,57 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define filas 5
+#define columnas 12
 
-int main(){
-    int simulation[5][12];
+int main()
+{
+    
+    int simulation[filas][columnas];
+    float promedioXano[filas];
+    int aux = 0;
     srand(time(NULL));
-    for (int i = 0; i < 5; i++)
+    // Aleatorio
+    for (int i = 0; i < filas; i++)
     {
-        for (int j = 0; j < 12; j++)
+        for (int j = 0; j < columnas; j++)
         {
-            simulation[i][j]=10000 + rand() % (50000 - 10000 + 1);
+            simulation[i][j] = 10000 + rand() % (50000 - 10000 + 1);
         }
     }
-    for (int i = 0; i < 5; i++)
-    { 
-        for (int j = 0; j < 12; j++)
+    //
+    // Mostrar Aleatorios
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
         {
-            printf("%d - ",simulation[i][j]);
-            
+            printf("%d - ", simulation[i][j]);
         }
         printf("\n");
+    }
+    //
+    // Armar promedio
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            
+                 promedioXano[i] = promedioXano[i] + simulation[i][j];
+        }
+    }
+    for (int i = 0; i < filas; i++)
+    {
+        promedioXano[i] = promedioXano[i] / 12;
+    }
+    
+    // Mostrar promedio
+    for (int k = 0; k < filas; k++)
+    {
+        if( k < 1)
+        {
+            printf("Promedio por año: %.2f ", promedioXano[k]);
+        }
+        printf(" %.2f ", promedioXano[k]);
     }
 
     return 0;
